3ab9ab7ffc38e044c07a98441e128d94d60ee919
Truong Dung <truonghoangdung1987hp@gmail.com>
2022-07-16 10:50:47 +0700

    Fix

--- medusa-config.js
+++ medusa-config.js
@@ -23,7 +23,7 @@ try {
 }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || "https://fanciful-muffin-7a04dd.netlify.app/,http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = "https://fanciful-muffin-7a04dd.netlify.app";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "https://storefront-truongteam.vercel.app,http://localhost:8000";
