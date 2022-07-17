30e392ae08db02392129a2fdac076afa5353c745
Truong Dung <truonghoangdung1987hp@gmail.com>
2022-07-17 21:01:01 +0700

    Add review

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,48 @@
 import { Router } from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import { projectConfig } from "../../medusa-config"
 
 export default () => {
   const router = Router()
+  const storeCorsOptions = {
+    origin: projectConfig.store_cors.split(","),
+    credentials: true,
+  }
 
-  router.get("/hello", (req, res) => {
-    res.json({
-      message: "Welcome to Your Store!",
+  router.get("/store/products/:id/reviews", cors(storeCorsOptions), (req, res) => {
+    const productReviewService = req.scope.resolve("productReviewService")
+    productReviewService.getProductReviews(req.params.id).then((product_reviews) => {
+      return res.json({
+        product_reviews
+      })
     })
   })
 
-  return router
+  router.use(bodyParser.json())
+  router.options("/store/products/:id/reviews", cors(storeCorsOptions))
+  router.post("/store/products/:id/reviews", cors(storeCorsOptions), (req, res) => {
+    const productReviewService = req.scope.resolve("productReviewService")
+    productReviewService.addProductReview(req.params.id, req.body.data).then((product_review) => {
+      return res.json({
+        product_review
+      })
+    })
+  })
+
+  const corsOptions = {
+    origin: projectConfig.admin_cors.split(","),
+    credentials: true,
+  }
+  router.options("/admin/products/:id/reviews", cors(corsOptions))
+  router.get("/admin/products/:id/reviews", cors(corsOptions), async (req, res) => {
+    const productReviewService = req.scope.resolve("productReviewService")
+    productReviewService.getProductReviews(req.params.id).then((product_reviews) => {
+      return res.json({
+        product_reviews
+      })
+    })
+  })
+
+  return router;
 }
